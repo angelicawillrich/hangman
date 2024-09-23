1b4313b26d9932c1fde43c77352910f1bd60dd26
Angelica Alves Willrich <angelica@willri.ch>
2024-09-23 10:17:24 +0200

    Initial commit with project based on a Web Dev Simplified project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# hangman
+# Hangman
+
+This project is based on a [Web Dev Simplified project](https://www.youtube.com/watch?v=-ONUyenGnWw&t=223s) project.
+
---
+++ src/Keyboard.tsx
@@ -0,0 +1,63 @@
+import styles from "./Keyboard.module.css"
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+]
+type KeyboardProps = {
+    activeLetters: string[]
+    inactiveLetters: string[]
+    disabled: boolean
+    addGuessLetter: (letter: string) => void
+}
+
+export function Keyboard({ activeLetters, inactiveLetters, disabled = false, addGuessLetter }: KeyboardProps) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map(key => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+        return (
+        <button
+            onClick={() => addGuessLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""}`}
+            key={key}
+            disabled={isInactive || isActive || disabled}
+        >
+            {key}
+        </button>
+    )
+      })}
+    </div>
+  )
+}
